1be5dce476147fa335214570ac26e5839f6feba7
Mohit Sharma <msharma3@me.iitr.ac.in>
2021-01-03 20:04:17 +0530

    Binary Search v1.0

---
+++ 2. Analysis of Algorithms/BinarySearch.java
@@ -0,0 +1,28 @@
+/**
+* Binary Search: Given a sorted array and a key, find index of key in array
+* Analysis of Method: O(lg N).
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   03-01-2021
+*/
+
+public class BinarySearch {
+
+	//If key appears in the array a[] -> a[lo]<a[mid]<a[hi]
+	public static int Binary_Search(int [] a, int key)
+	{
+		int lo=0;
+		int hi=a.length-1;
+		while(lo<=hi)
+		{
+			//one 3-way compare
+			int mid=lo+(hi-lo)/2;
+			if(key<a[mid])	hi=mid-1;
+			else if(key>a[mid])	lo=mid+1;
+			else return mid;
+		}
+		
+		return -1;
+	}
+}
