3dae992f94204601ee55b36775202b0f5396548a
mojito <msharma3@me.ittr.ac.in>
2021-01-04 19:03:05 +0530

    Stack of Strings v1.0 (Linked-List)

---
+++ 3. Stacks and Queues/LinkedStackofStrings.java
@@ -0,0 +1,73 @@
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+* Stack: A data structure used to store objects. It works on the principle that it examines the elements least recently
+* entered: LIFO (Last in, First out)
+* 
+* @category Linked List approach to the implementation of Stack of Strings.
+* @author  Mohit Sharma
+* @version 1.0
+* @since   04-01-2021
+*/
+
+public class LinkedStackofStrings {
+	
+	private Node first;
+	private int N;
+	
+	private class Node
+	{
+		String item;
+		Node next;
+	}
+	
+	public LinkedStackofStrings()	//create an empty stack
+	{
+		first=null;
+		N=0;
+	}
+	
+	public void push(String item) //insert a new string onto stack
+	{
+		N++;
+		Node oldfirst=first;
+		first=new Node();
+		first.item=item;
+		first.next=oldfirst;
+	}
+	
+	public String pop() //remove and return the string
+	{
+		if(!isEmpty())
+		{	
+			N--;
+			String item=first.item;
+			first=first.next;
+			return item;
+		}
+		else
+			return "-1";
+			
+	}
+
+	public boolean isEmpty() //is the stack empty?
+	{
+		return first==null;
+	}
+	
+	public int size()	//number of strings on the stack
+	{
+		return N;
+	}
+	
+	public static void main(String [] args) //tester
+	{
+		LinkedStackofStrings stack=new LinkedStackofStrings();
+		while(!StdIn.isEmpty())
+		{
+			String s=StdIn.readString();
+			if(s.equals("-"))	System.out.println(stack.pop());
+			else				stack.push(s);
+		}
+	}
+}
