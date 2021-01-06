16e95da015ecc0e743aaf0a7209082a73e98295b
mojito <msharma3@me.ittr.ac.in>
2021-01-06 17:27:45 +0530

    Merge Sort v2.0 (Improved)

--- 5. Merge Sort/Merge.java
+++ 5. Merge Sort/Merge.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.Insertion;
+
 /**
 * <h1>Merge Sort: Critical component in the world's computational infrastructure. Java uses this to sort for objects.</h1>
 * 
@@ -7,9 +9,10 @@
 * <li>Analysis: O(NlgN) compares with O(N) auxiliary space
 * <li>Analysis: ~NlgN compares & ~6NlgN array exchanges
 * <li>Merge Sort is stable
+* <li>Only N-1 compares used if optimization used: a[mid]<=a[mid+1] --> return
 * 
 * @author  Mohit Sharma
-* @version 1.0
+* @version 2.0
 * @since   05-01-2021
 */
 
@@ -25,7 +28,8 @@ public class Merge {
 			aux[k]=a[k];
 		
 		int i=lo;	int j=mid+1;
-		for(int k=lo;k<=hi;k++) {
+		for(int k=lo;k<=hi;k++) 
+		{
 			if(i>mid)						a[k]=aux[j++];
 			else if(j>hi)					a[k]=aux[i++];
 			else if(less(aux[j],aux[i]))	a[k]=aux[j++];
@@ -42,10 +46,16 @@ public class Merge {
 	
 	private static void sort(Comparable [] a, Comparable [] aux, int lo, int hi)
 	{
-		if(hi<=lo)	return;
+		if(hi<=lo+7-1) //cutoff to Insertion sort for array size less than 7	
+		{
+			Insertion.sort(a,lo,hi);
+			return;
+		}
 		int mid=lo+(hi-lo)/2;
 		sort(a,aux,lo,mid);
 		sort(a,aux,mid+1,hi);
+		if(!less(a[mid+1],a[mid])) //Helps for partially sorted arrays.
+			return;
 		merge(a,aux,lo,mid,hi);
 	}
 	
@@ -54,4 +64,29 @@ public class Merge {
 		Comparable [] aux=new Comparable[a.length];
 		sort(a,aux,0,a.length-1);
 	}
+	
+	/*Eliminate the copy of Auxiliary array, by switching the role of the input and the output in each recursive call.
+	 * 
+	 * private static void merge(a,aux,lo,mid,hi)
+	 * {
+	 * 		----------
+	 * 		----------
+	 * 		{	
+	 * 			if(i>mid)					aux[k]=a[j++];
+	 * 			else if(j>hi)				aux[k]=a[i++];
+	 * 			else if(less(a[j],a[i])		aux[k]=a[j++];
+	 * 			else if(less(a[i],a[j])		aux[k]=a[i++];
+	 * 		}
+	 * }
+	 * 
+	 * private static void sort(a,aux,lo,mid,hi)
+	 * {
+	 * 		----------
+	 * 		----------
+	 * 		sort(aux,a,lo,mid);
+	 * 		sort(aux,a,mid+1,hi);
+	 * 		merge(a,aux,lo,mid,hi);
+	 * }
+	 * 
+	 */
 }
