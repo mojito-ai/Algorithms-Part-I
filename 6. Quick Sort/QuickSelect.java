1078ac3e0aeddb403e4e357eb31e0fab2a38cafb
mojito <msharma3@me.ittr.ac.in>
2021-01-07 21:25:42 +0530

    Quick-Select v1.0 (Ordered Statistics)

---
+++ 6. Quick Sort/QuickSelect.java
@@ -0,0 +1,70 @@
+import edu.princeton.cs.algs4.StdRandom;
+/**
+* <h1>Quick Select:Given an array of N items, find the kth largest element</h1>
+* 
+* <li>Plan: Partition array same as Quick Sort
+* <li>Repeat in one sub-array, depending on j, finished when j equals k
+* 
+* <br>Performance Analysis:
+* <li>Used in order statistics
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   07-01-2021
+*/
+
+@SuppressWarnings("rawtypes")
+public class QuickSelect {
+
+	public static Comparable select(Comparable [] a, int k)
+	{
+		StdRandom.shuffle(a);
+		int lo=0;	int hi=a.length-1;
+		while(hi>lo)
+		{
+			int j=partition(a,lo,hi);
+			if(j<k)
+				lo=j+1;
+			else if(j>k)
+				hi=j-1;
+			else 
+				return a[k];
+		}
+		return a[k];
+	}
+
+	private static int partition(Comparable [] a, int lo, int hi)
+	{
+		int i=lo;	int j=hi+1;
+		while(true)
+		{
+			while(less(a[++i],a[lo]))
+				if(i==hi)
+					break;
+			
+			while(less(a[lo],a[--j]))
+				if(j==lo)
+					break;
+			
+			if(j<=i)
+				break;
+			
+			exch(a,i,j);
+		}
+		exch(a,lo,j);
+		return j;
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static boolean less(Comparable v, Comparable w)
+	{
+		return v.compareTo(w)<0;
+	}
+	
+	private static void exch(Comparable [] a, int i, int j)
+	{
+		Comparable swap=a[i];
+		a[i]=a[j];
+		a[j]=swap;
+	}
+}
