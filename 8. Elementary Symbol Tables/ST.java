7ef74d7fab73ae61cf1de8a0d278db47a80f24d5
mojito <msharma3@me.ittr.ac.in>
2021-01-09 17:35:29 +0530

    Symbol Table API

---
+++ 8. Elementary Symbol Tables/ST.java
@@ -0,0 +1,33 @@
+/**
+* <h1>Symbol Table: Insert a value with specified key. Given a key, search for the corresponding value.</h1>
+* 
+* <li>Associative array: Associate one value with each key
+* <li>Applications: Purpose of Search=Dictionary | Key=word | Value=definition
+* <li>Purpose of Search=Financial Account | Key=Account number | Value=Transaction details
+* <li>Assume Keys are comparable, use compareTo()
+* <li>Assume Keys are generic types, use equals() to test equality. Use hashCode() to scramble key.
+* <li>Use immutable keys for symbol table keys.
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   09-01-2021
+* 
+*/
+
+public class ST<Key extends Comparable<Key>,Value> 
+{
+	ST() 					//Create an ordered Symbol Table
+	{
+		
+	}
+	
+	void put(Key key, Value val)	//put key-value pair in the table
+	{
+		
+	}
+	
+	Value get(Key key)				//Value passed with Key(Null if key is absent
+	{
+		
+	}
+}
