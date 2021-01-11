e627a39a5395a996694eb576aba27ffc6f3d90ec
mojito <msharma3@me.ittr.ac.in>
2021-01-11 12:51:48 +0530

    KdTrees

---
+++ 10. Geometric Applications of BSTs/KdTrees.java
@@ -0,0 +1,41 @@
+/**
+* <h1>Kd-Trees: Extension of BSTs that allow us to do effecient processing of sets of points in space</h1>
+*
+* <li> Extension of ordered symbol tables
+* <li> Insert a 2d key
+* <li> Search for a 2d key
+* <li> Delete a 2d key
+* <li> Range Search: Find all keys that lie in 2d range
+* <li> Range Count: Number of keys that lie in a 2d range
+* 
+* <li>Application: Networking, Circuit Design, Databases
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   11-01-2021
+* 
+*/
+
+public class KdTrees {
+	
+	/**
+	 * Keys are points in a plane.
+	 * Find/Count points in a given h-v rectangle.
+	 * 
+	 */
+	
+	/*Grid Implementation: 2d orthogonal range search
+	 * 
+	 * Divide space into M*M grid of squares.
+	 * Create list of points in each square.
+	 * Use 2d array to directly index the relevant square.
+	 * Insert (x,y) to list for particular square.
+	 * Range Search: Examine only squares that intersect 2-D range search query.
+	 * Space = M^2 + N
+	 * Time = 1 + N/M^2
+	 * Rule of thumb: N^0.5 * N^0.5 grid
+	 * 
+	 * Caveat: Clustering
+	 */
+
+}
