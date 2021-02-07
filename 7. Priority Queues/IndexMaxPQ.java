b5caaef0608d1617071a24cbb3dddeb436d45f2f
mojito <msharma3@me.ittr.ac.in>
2021-02-07 22:31:06 +0530

    IndexMaxPQ (MaxPQ construction)

--- 7. Priority Queues/IndexMaxPQ.java
+++ 7. Priority Queues/IndexMaxPQ.java
@@ -17,6 +17,62 @@
 * 
 */
 
-public class IndexMaxPQ {
+public class IndexMaxPQ<Key extends Comparable<Key>> {
+	
+	private Key [] pq;
+	private int N=0;
+	@SuppressWarnings("unchecked")
+	IndexMaxPQ(int N)
+	{
+		pq=(Key []) new Object[N+1];
+	}
+	
+	public void insert(Key key)
+	{
+		pq[++N]=key;
+		swim(N);
+	}
+	private void swim(int k)
+	{
+		while(k>1 && less(k/2,k))
+		{
+			exch(k,k/2);
+			k=k/2;
+		}
+	}
+	
+	public Key deleteMax()
+	{
+		Key key=pq[1];
+		exch(1,N);
+		sink(1);
+		pq[N--]=null;
+		return key;
+	}
+	private void sink(int k)
+	{
+		while(N>=2*k)
+		{
+			int j=2*k;
+			if(j<N && less(j,j+1))
+				j=j+1;
+			if(!less(k,j))
+				break;
+			exch(k,j);
+			k=j;
+		}
+	}
+	
+	private boolean less(int i, int j)	//helper functions
+	{
+		return pq[i].compareTo(pq[j])<0;
+	}
+	
+	private void exch(int i, int j)	
+	{
+		Key swap=pq[i];
+		pq[i]=pq[j];
+		pq[j]=swap;
+	}
 
 }
