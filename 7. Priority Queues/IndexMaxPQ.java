8c91eaa602ced0852d1b06232d655e93397b0330
mojito <msharma3@me.ittr.ac.in>
2021-02-07 22:05:39 +0530

    IndexMaxPQ v1.0

---
+++ 7. Priority Queues/IndexMaxPQ.java
@@ -0,0 +1,22 @@
+/**
+* <h1>Indexed Max Priority Queue: It is a traditional priority queue variant which on top of the regular PQ operations
+* 								  supports quick updates and deletions of key-value pairs. </h1>
+* 
+* <li> Associate an index between 0 & N-1 with each key in a priority queue.
+* <li> Client can insert and delete-the-minimum.
+* <li> Client can change the key by specifying the index.
+* <li> Suppose we have N people with different priorities that we need to serve. Assume priorities can change dynamically
+* 	   and we always want to serve the person with the highest priority. To figure out who to serve next, we use a max IPQ
+* 	   to sort by lowest value first.
+* <li> Use Hash-table to convert key values to integer based.
+* 
+* 
+* @author  Mohit Sharma
+* @version 1.0
+* @since   07-02-2021
+* 
+*/
+
+public class IndexMaxPQ {
+
+}
